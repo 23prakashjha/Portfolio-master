df18db6c232de957cafefeb9469eb7eeb10b4bee
Your Name <you@example.com>
2026-07-06 14:45:21 +0530

    commit first

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -41,7 +41,7 @@ const Hero = () => {
 
           <a
             className="hero-resume"
-            href="/Prakash Jha.pdf"
+            href="/Prakash_Jha_Mern Stack Developer.pdf"
             target="_blank"
             rel="noopener noreferrer"
           >
